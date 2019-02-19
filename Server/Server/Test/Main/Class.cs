9e6bc73abb62649c6a805c4e956a568f3af9e902
Lansional <cbg1720@gmail.com>
2019-02-19 17:41:02 +0900

    만들었다 에러 뜨지만.....

---
+++ Server/Server/Test/Main/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy.Hosting.Self;
+
+namespace Server.Test.Main
+{
+    static NancyHost Host;
+
+    static void Main(string[] args)
+    {
+        string url = ConfigurationManager.AppSettings["Url"];
+        Host = new NancyHost(new Uri(url));
+        Host.Start();
+        Console.WriteLine("서버 실행했습니다, 서버 주소는: {0}", url);
+        Console.ReadLine();     // 여기에서 반복문 써서 콘솔창에서 조작
+    }
+}
